aad7b89998f1f8358a4b95da4a8ed4657c67271a
Prisca-Tiana <prisca.tiana.r@gmail.com>
2023-05-05 09:51:24 +0300

    load eneignant

--- App.js
+++ App.js
@@ -1,5 +1,35 @@
 import React from 'react';
-import { FlatList, Text, View } from 'react-native';
+import { FlatList, Text, View, StyleSheet, TextInput, Button, 
+  Plateform, TouchableOpacity, Alert, ActivityIndicator } from 'react-native';
+import { Component } from 'react';
+import { StackNavigator } from 'react-native';
+
+const API_URL = '10.10.0.2:8000/api/enseignants';
+
+class MainActivity extends Component {
+  static navigationOptions = {
+    title : 'Enseignant',
+  }
+
+  constructor(props){
+    super(props)
+    this.state = {
+      // ato no asiana ny élément
+    }
+  }
+
+  loadEnseignant = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      setEnseignant(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+}
 
 const persons = [
   { id: 1, name: 'John' },
