5e41a73df7e4ea355412a86770c08c27b5a72928
Prisca-Tiana <prisca.tiana.r@gmail.com>
2023-05-05 11:44:20 +0300

    front done

--- App.js
+++ App.js
@@ -18,6 +18,7 @@ class MainActivity extends Component {
     }
   }
 
+  // ty mount ty tsy sure ah hoe ahoana no hamenoana azy satria tsy mitovy am nen ramose ty
   componentDidMount(){
     this.loadEnseignant
   }
@@ -41,15 +42,8 @@ class MainActivity extends Component {
 
   }
 
-  removeEnseignant = async id => {
-    try {
-      await fetch(`\${API_URL}/\${id}`, { method: 'DELETE' });
-      setEnseignant(enseignants => enseignants.filter(p => p.id !== id));
-    } catch (error) {
-      console.error(error);
-    }
-  }
 
+// mi-afficher ny total an'ny prestation rehetra fa tsy haiko hoe ampidirina aiza
   loadPrestationTotal = async () => {
     try {
       const response = await fetch(API_URL/total);
@@ -60,12 +54,7 @@ class MainActivity extends Component {
     }
   }
 
-  onRefresh = async () => {
-    setRefreshing(true);
-    await loadEnseignant();
-    setRefreshing(false);
-  }
-
+  //maka ny enseignant ray
   getEnseignant = async (idEnseignant, nomEnseignant, tauxHoraire, nb_heure) => {
     this.props.navigation.navigate('Enseignant'), {
       ID : idEnseignant,
@@ -116,9 +105,6 @@ class MainActivity extends Component {
             <TouchableOpacity onPress={() => this.editEnseignant(item.id)}>
                 <Text>Modifier</Text>
              </TouchableOpacity>
-            <TouchableOpacity onPress={() => this.removeEnseignant(item.id)}>
-                <Text>Supprimer</Text>
-            </TouchableOpacity>
         </View>
       </View>
       
@@ -127,7 +113,11 @@ class MainActivity extends Component {
 
 }
 
+// add enseignant 
 class AddEnseignantActivity extends Component {
+  static navigationOptions = {
+    title : 'Ajout Enseignant'
+  }
   constructor(props) {
     super(props);
     this.state = {
@@ -167,7 +157,7 @@ class AddEnseignantActivity extends Component {
   render() {
     return (
       <View style={styles.MainContainer}>
-        <Text style={{fontSize: 20, textAlign: 'center', marginBottom: 7}}> Ajout d'enseignant </Text>
+        <Text style={{fontSize: 20, textAlign: 'center', marginBottom: 7}}> Formualaire d'ajout </Text>
         <TexInput 
           placeholder='Matricule Enseignant' 
           onChangeText={ TextInputValue => this.setState({ TextInput_Enseignant_Matricule: TextInputValue})}
@@ -201,7 +191,11 @@ class AddEnseignantActivity extends Component {
   }
 }
 
+// edit enseignant 
 class EditEnseignantActvity extends Component {
+  static navigationOptions = {
+    title : 'Modification Enseignant'
+  }
   constructor(props) {
     super(props);
     this.state = {
@@ -211,26 +205,89 @@ class EditEnseignantActvity extends Component {
       TextInput_Enseignant_nb_Heure: '',
     }
   }
+
+  componentDidMount(){
+    this.setState({
+      TextInput_Enseignant_Matricule : this.props.navigation.state.params.MATRICULE,
+      TextInput_Enseignant_Nom : this.props.navigation.state.params.NOM,
+      TextInput_Enseignant_Taux_Horaire : this.props.navigation.state.params.TAUX_HORAIRE,
+      TextInput_Enseignant_nb_Heure : this.props.navigation.state.params.NB_HEURE,
+    })
+  }
+
+  updateEnseignantRecord = async () => {
+    try {
+      const response = await fetch(`\${API_URL}/\${updatedEnseignant.id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(updatedEnseignant)
+      }).then((response => response.json())
+        .then((responseJson) => {
+        Alert.alert(responseJson)
+      }).catch((error) => {
+        console.error(error)
+      })
+      )
+      const data = await response.json();
+      setEnseignant(enseignants => enseignants.map(e => e.id === data.id ? data : e));
+      setEditingEnseignant(null);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  removeEnseignantRecord = async (id) => {
+    try {
+      await fetch(`\${API_URL}/\${id}`, { method: 'DELETE' });
+      setEnseignant(enseignants => enseignants.filter(p => p.id !== id));
+    } catch (error) {
+      console.error(error);
+    }
+    //  mety mila ovaina ty first ty - de rah ovaina de titre anle main activity : Enseignant
+    this.props.navigation.navigate('First')
+  }
+
+  render() {
+    return (
+      <View style={styles.MainContainer}>
+        <Text style={{fontSize: 20, textAlign: 'center', marginBottom: 7}}> Formualaire de modification </Text>
+        <TexInput 
+          placeholder='Matricule Enseignant' 
+          onChangeText={ TextInputValue => this.setState({ TextInput_Enseignant_Matricule: TextInputValue})}
+          underlineColorAndroid='transparent'
+          value={this.state.TextInput_Enseignant_Matricule}
+          style = {styles.TextInputSylteClass}
+        />
+        <TexInput 
+          placeholder='Nom Enseignant' 
+          onChangeText={ TextInputValue => this.setState({ TextInput_Enseignant_Nom: TextInputValue})}
+          underlineColorAndroid='transparent'
+          value={this.state.TextInput_Enseignant_Matricule}
+          style = {styles.TextInputSylteClass}
+        />
+        <TexInput 
+          placeholder='Taux horaire Enseignant' 
+          onChangeText={ TextInputValue => this.setState({ TextInput_Enseignant_Taux_Horaire: TextInputValue})}
+          underlineColorAndroid='transparent'
+          value={this.state.TextInput_Enseignant_Matricule}
+          style = {styles.TextInputSylteClass}
+        />
+        <TexInput 
+          placeholder="Nombre d'heure Enseignant"
+          onChangeText={ TextInputValue => this.setState({ TextInput_Enseignant_nb_Heure: TextInputValue})}
+          underlineColorAndroid='transparent'
+          value={this.state.TextInput_Enseignant_Matricule}
+          style = {styles.TextInputSylteClass}
+        />
+        <TouchableOpacity activeOpacity={ .8 }
+          onPress={this.updateEnseignantRecord}>
+            <Text>Modifier</Text>
+          </TouchableOpacity>
+        <TouchableOpacity activeOpacity={ .8 }
+          onPress={this.removeEnseignantRecord}>
+            <Text>Supprimer</Text>
+          </TouchableOpacity>
+      </View>
+    )
+  }
 }
-
-const persons = [
-  { id: 1, name: 'John' },
-  { id: 2, name: 'Jane' },
-  { id: 3, name: 'Bob' },
-];
-
-const PersonList = () => {
-  const renderItem = ({ item }) => <Text>{item.name}</Text>;
-
-  return (
-    <View>
-      <FlatList
-        data={persons}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
-      />
-    </View>
-  );
-};
-
-export default PersonList;
